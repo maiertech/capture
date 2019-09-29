80d81e55b92cfca17b88c0dc2c4d9deda8e0d717
Thilo Maier <git@maier.asia>
2019-09-29 15:16:08 -0400

    Verify that url exists #5

--- test/capture.test.js
+++ test/capture.test.js
@@ -1,16 +1,15 @@
 const capture = require('../api/capture');
 
-test('invalid url param', () => {
+test('invalid url (invalid syntax)', async () => {
   const params = {
     url: 'invalid',
     device: 'iPhone X',
   };
   const request = {
-    method: 'POST',
     body: params,
   };
   const response = { json: jest.fn(), status: jest.fn() };
-  capture(request, response);
+  await capture(request, response);
   expect(response.status).toHaveBeenCalledTimes(1);
   expect(response.status).toHaveBeenCalledWith(400);
   expect(response.json).toHaveBeenCalledTimes(1);
@@ -23,17 +22,38 @@ test('invalid url param', () => {
   ]);
 });
 
-test('invalid device param', () => {
+test('invalid url (unresolvable)', async () => {
+  const params = {
+    url: 'https://invalid.maier.tech',
+    device: 'iPhone X',
+  };
+  const request = {
+    body: params,
+  };
+  const response = { json: jest.fn(), status: jest.fn() };
+  await capture(request, response);
+  expect(response.status).toHaveBeenCalledTimes(1);
+  expect(response.status).toHaveBeenCalledWith(400);
+  expect(response.json).toHaveBeenCalledTimes(1);
+  expect(response.json).toHaveBeenCalledWith([
+    {
+      param: 'url',
+      value: 'https://invalid.maier.tech',
+      message: 'Invalid URL.',
+    },
+  ]);
+});
+
+test('invalid device param', async () => {
   const params = {
     url: 'https://www.google.com/',
     device: 'invalid',
   };
   const request = {
-    method: 'POST',
     body: params,
   };
   const response = { json: jest.fn(), status: jest.fn() };
-  capture(request, response);
+  await capture(request, response);
   expect(response.status).toHaveBeenCalledTimes(1);
   expect(response.status).toHaveBeenCalledWith(400);
   expect(response.json).toHaveBeenCalledTimes(1);
@@ -45,67 +65,3 @@ test('invalid device param', () => {
     },
   ]);
 });
-
-/*
-test('valid device', async () => {await new Promise(t => {
-  const response = await axios.post(localhost, {
-    device: 'iPhone X',
-    url: 'https://google.com',
-  });
-  t.is(response.status, 200);
-})});
-
-test('valid URL: https', async () => {await new Promise(t => {
-  const response = await axios.post(localhost, {
-    device: 'iPhone X',
-    url: 'https://google.com',
-  });
-  t.is(response.status, 200);
-})});
-
-test('valid URL: http', async () => {await new Promise(t => {
-  const response = await axios.post(localhost, {
-    device: 'iPhone X',
-    url: 'http://google.com',
-  });
-  t.is(response.status, 200);
-})});
-
-test('invalid URL: localhost', async () => {await new Promise(t => {
-  try {
-    await axios.post(localhost, {
-      device: 'iPhone X landscape',
-      url: 'http://localhost:8000',
-    });
-    // If axios does not throw error, fail test.
-    t.fail();
-  } catch (error) {
-    t.is(error.response.status, 422);
-    t.deepEqual(error.response.data.errors[0], {
-      location: 'body',
-      msg: 'Invalid value',
-      param: 'url',
-      value: 'http://localhost:8000',
-    });
-  }
-})});
-
-test('invalid URL: typo', async () => {await new Promise(t => {
-  try {
-    await axios.post(localhost, {
-      device: 'iPhone X landscape',
-      url: 'https:/maier.tech',
-    });
-    // If axios does not throw error, fail test.
-    t.fail();
-  } catch (error) {
-    t.is(error.response.status, 422);
-    t.deepEqual(error.response.data.errors[0], {
-      location: 'body',
-      msg: 'Invalid value',
-      param: 'url',
-      value: 'https:/maier.tech',
-    });
-  }
-})});
-*/

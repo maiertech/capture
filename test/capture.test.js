f17b7c0094062356922f30876eb9fe6aef52c83f
Thilo Maier <git@maier.asia>
2019-11-24 20:36:42 -0500

    Update dependency eslint-plugin-shopify to v33

--- test/capture.test.js
+++ test/capture.test.js
@@ -1,69 +1,73 @@
 const http = require('http');
+
 const listen = require('test-listen');
 const axios = require('axios');
+
 const capture = require('../api/capture');
 
 let server;
 let localhost;
 
-beforeAll(async () => {
-  server = http.createServer(capture);
-  localhost = await listen(server);
-});
-
-// Workaround for https://github.com/facebook/jest/issues/8554.
-afterAll(done => {
-  server.close(() => {
-    setTimeout(done, 100);
+describe('capture API', () => {
+  beforeAll(async () => {
+    server = http.createServer(capture);
+    localhost = await listen(server);
   });
-});
 
-test('invalid url param', async () => {
-  expect.assertions(1);
-  try {
-    await axios.post(localhost, {
-      url: 'invalid',
-      device: 'iPhone X',
+  // Workaround for https://github.com/facebook/jest/issues/8554.
+  afterAll(done => {
+    server.close(() => {
+      setTimeout(done, 100);
     });
-  } catch (error) {
-    expect(error.response.data).toStrictEqual([
-      { param: 'url', value: 'invalid', message: 'Invalid URL syntax' },
-    ]);
-  }
-});
+  });
 
-test('unresolvable url param', async () => {
-  expect.assertions(1);
-  try {
-    await axios.post(localhost, {
-      url: 'https://invalid.maier.tech',
-      device: 'iPhone X',
-    });
-  } catch (error) {
-    expect(error.response.data).toStrictEqual([
-      {
-        param: 'url',
-        value: 'https://invalid.maier.tech',
-        message: 'Cannot resolve URL (Request failed with status code 404)',
-      },
-    ]);
-  }
-});
+  it('invalid url param', async () => {
+    expect.assertions(1);
+    try {
+      await axios.post(localhost, {
+        url: 'invalid',
+        device: 'iPhone X',
+      });
+    } catch (error) {
+      expect(error.response.data).toStrictEqual([
+        { param: 'url', value: 'invalid', message: 'Invalid URL syntax' },
+      ]);
+    }
+  });
 
-test('invalid device param', async () => {
-  expect.assertions(1);
-  try {
-    await axios.post(localhost, {
-      url: 'https://www.google.com/',
-      device: 'invalid',
-    });
-  } catch (error) {
-    expect(error.response.data).toStrictEqual([
-      {
-        param: 'device',
-        value: 'invalid',
-        message: 'Invalid device',
-      },
-    ]);
-  }
+  it('unresolvable url param', async () => {
+    expect.assertions(1);
+    try {
+      await axios.post(localhost, {
+        url: 'https://invalid.maier.tech',
+        device: 'iPhone X',
+      });
+    } catch (error) {
+      expect(error.response.data).toStrictEqual([
+        {
+          param: 'url',
+          value: 'https://invalid.maier.tech',
+          message: 'Cannot resolve URL (Request failed with status code 404)',
+        },
+      ]);
+    }
+  });
+
+  it('invalid device param', async () => {
+    expect.assertions(1);
+    try {
+      await axios.post(localhost, {
+        url: 'https://www.google.com/',
+        device: 'invalid',
+      });
+    } catch (error) {
+      expect(error.response.data).toStrictEqual([
+        {
+          param: 'device',
+          value: 'invalid',
+          message: 'Invalid device',
+        },
+      ]);
+    }
+  });
 });
